967537de515b73b075ff73090a37137ccfb41036
weilong000827@gmail.com <weilong000827@gmail.com>
2026-03-15 01:38:34 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,13 +4,11 @@
 variable "region" {
   type        = string
   description = "AWS region for all resources."
-
   default = "us-east-1"
 }
 
 variable "project_name" {
   type        = string
   description = "Name of the example project."
-
   default = "terraform-init"
 }
